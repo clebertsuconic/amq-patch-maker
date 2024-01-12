c068ff89df37e870a4ca7f39a003b56d3dbdf4df
Clebert Suconic <clebertsuconic@apache.org>
2024-01-12 09:11:36 -0500

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM registry.redhat.io/amq7/amq-broker-rhel8:7.11.0
+COPY lib /opt/amq/lib
+# Define the user
+USER 185
+# Define the working directory
+WORKDIR /home/jboss
+# Define run cmd
+CMD ["/opt/amq/bin/launch.sh", "start"]
---
+++ build-patch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <bin-folder> <version>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+bin_folder=$1
+version=$2
+
+# Check if the bin folder exists
+if [ ! -d "$bin_folder" ]; then
+    echo "Error: Bin folder '$bin_folder' does not exist."
+    exit 1
+fi
+
+rm -rf target
+cp -r $bin_folder ./target
+cp Dockerfile target
+cd target
+
+
+docker build --tag amq-broker-rhel8:$version .
+docker save amq-broker-rhel8:$version -o ../$version.tar.gz
